edbcdc77b75376ef9b6a31bfd1434329ecaccb96
Soroush Habibi <soroush.habibi.1382@gmail.com>
2023-02-16 17:54:37 +0330

    add error handling middleware and use db.ts to verify token

--- build/bot.js
+++ build/bot.js
@@ -5,7 +5,7 @@ const bot = new grammy.Bot("5991825741:AAGzDG7sIV90vU_5vNSVmh0506gO8lNz53I");
 const users = [];
 //!----------------------keyboards----------------------!//
 const customKeyboard = new grammy.Keyboard()
-    .text("status")
+    .text("server status")
     .persistent()
     .resized();
 //!----------------------middlewares----------------------!//
@@ -19,18 +19,18 @@ bot.use((ctx, next) => {
 bot.command("start", (ctx) => {
     let userObj = getChatObject(ctx.chat.id);
     if (!userObj) {
-        ctx.reply("Welcome.please enter your login token:");
+        ctx.reply("Welcome.please enter your login token:", { reply_markup: { remove_keyboard: true } });
         const newUser = new user(ctx.chat.id);
         users.push(newUser);
     }
     else if (!userObj.token) {
-        ctx.reply("Please enter your token:");
+        ctx.reply("Please enter your token:", { reply_markup: { remove_keyboard: true } });
     }
     else if (userObj.token) {
         ctx.reply("You have loged in", { reply_markup: customKeyboard });
     }
 });
-bot.hears("status", (ctx) => {
+bot.hears("server status", (ctx) => {
     if (getChatObject(ctx.chat.id)) {
         ctx.reply("Ok");
     }
@@ -39,11 +39,19 @@ bot.hears("status", (ctx) => {
     }
 });
 //!----------------------events----------------------!//
-bot.on("message", (ctx) => {
+bot.on("message", async (ctx) => {
     const user = getChatObject(ctx.chat.id);
     if (user && !user.token && ctx.message.text) {
-        ctx.reply("done!", { reply_markup: customKeyboard });
-        user.token = ctx.message.text;
+        if (!/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(ctx.message.text.trim())) {
+            ctx.reply("Token is invalid", { reply_markup: { remove_keyboard: true } });
+        }
+        else if (!(await db.getSub(ctx.message.text.trim()))) {
+            ctx.reply("Can not find token in database!", { reply_markup: { remove_keyboard: true } });
+        }
+        else {
+            ctx.reply("done!", { reply_markup: customKeyboard });
+            user.token = ctx.message.text.trim();
+        }
     }
 });
 //!----------------------functions----------------------!//
@@ -54,5 +62,9 @@ function getChatObject(chatId) {
         }
     }
 }
+//!----------------------error handling----------------------!//
+bot.catch((err) => {
+    console.log(err.message);
+});
 //!----------------------launch----------------------!//
 bot.start();
--- source/bot.ts
+++ source/bot.ts
@@ -10,7 +10,7 @@ const users: user[] = [];
 //!----------------------keyboards----------------------!//
 
 const customKeyboard = new grammy.Keyboard()
-    .text("status")
+    .text("server status")
     .persistent()
     .resized();
 
@@ -28,17 +28,17 @@ bot.use((ctx, next) => {
 bot.command("start", (ctx) => {
     let userObj = getChatObject(ctx.chat.id);
     if (!userObj) {
-        ctx.reply("Welcome.please enter your login token:");
+        ctx.reply("Welcome.please enter your login token:", { reply_markup: { remove_keyboard: true } });
         const newUser = new user(ctx.chat.id);
         users.push(newUser);
     } else if (!userObj.token) {
-        ctx.reply("Please enter your token:");
+        ctx.reply("Please enter your token:", { reply_markup: { remove_keyboard: true } });
     } else if (userObj.token) {
         ctx.reply("You have loged in", { reply_markup: customKeyboard });
     }
 });
 
-bot.hears("status", (ctx) => {
+bot.hears("server status", (ctx) => {
     if (getChatObject(ctx.chat.id)) {
         ctx.reply("Ok");
     } else {
@@ -48,11 +48,17 @@ bot.hears("status", (ctx) => {
 
 //!----------------------events----------------------!//
 
-bot.on("message", (ctx) => {
+bot.on("message", async (ctx) => {
     const user = getChatObject(ctx.chat.id);
     if (user && !user.token && ctx.message.text) {
-        ctx.reply("done!", { reply_markup: customKeyboard });
-        user.token = ctx.message.text;
+        if (!/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(ctx.message.text.trim())) {
+            ctx.reply("Token is invalid", { reply_markup: { remove_keyboard: true } });
+        } else if (!(await db.getSub(ctx.message.text.trim()))) {
+            ctx.reply("Can not find token in database!", { reply_markup: { remove_keyboard: true } });
+        } else {
+            ctx.reply("done!", { reply_markup: customKeyboard });
+            user.token = ctx.message.text.trim();
+        }
     }
 });
 
@@ -65,6 +71,11 @@ function getChatObject(chatId: number): user | void {
         }
     }
 }
+//!----------------------error handling----------------------!//
+
+bot.catch((err) => {
+    console.log(err.message);
+});
 
 //!----------------------launch----------------------!//
 
